10ca5b888ee2804c9a63cc71e019aa0213d21890
Oscar Garay <ojcgv21@gmail.com>
2017-11-25 19:15:34 +0100

    #3 CREATE TABLE Query + INSERT INTO Query

--- utils/sql.js
+++ utils/sql.js
@@ -1,12 +1,15 @@
-var isId = function (field) {
+const _ = require("underscore");
+const mysql = require("promise-mysql");
+
+const isId = (field) => {
     return field.name.slice(0, 2) === "id";
 };
 
-var isFk = function (field) {
+const isFk = (field) => {
     return field.name.slice(0, 2) === "fk";
 };
 
-var buildColumn = function (field) {
+const buildColumn = (field) => {
     if (isId(field)) {
         return '\n '.concat(field.name).concat(' INT NOT NULL AUTO_INCREMENT,');
     } else if (isFk(field)) {
@@ -16,12 +19,12 @@ var buildColumn = function (field) {
     }
 };
 
-var pk = function (pkName, query) {
+const pk = (pkName, query) => {
     return (pkName ? query.concat('\n '.concat('PRIMARY KEY ( ').concat(pkName).concat(' ),')) : query);
 };
 
-var fks = function (fkNames, query) {
-    fkNames.forEach(function (fk) {
+const fks = (fkNames, query) => {
+    fkNames.forEach((fk) => {
         var fk_table = fk.substring(3);
         var fk_id = 'id_'.concat(fk_table);
         query = query.concat('\n ')
@@ -32,11 +35,12 @@ var fks = function (fkNames, query) {
     return query;
 };
 
-exports.createTableQuery = function (table) {
-    var query = '';
+exports.createTableQuery = (table) => {
+    let query = '';
     query = query.concat('CREATE TABLE IF NOT EXISTS ').concat(table.table_name).concat('  (');
-    var pkName, fkNames = [];
-    table.fields.forEach(function (field) {
+    let pkName;
+    const fkNames = [];
+    table.fields.forEach((field) => {
         if (isId(field)) {
             pkName = field.name;
         } else if (isFk(field)) {
@@ -48,5 +52,44 @@ exports.createTableQuery = function (table) {
     query = fks(fkNames, query);
     query = query.slice(0, -1);
     query = query.concat(')');
+    console.info(query);
     return query;
+};
+
+exports.insertIntoQuery = (table_name, objs) => {
+    if (objs.length === 0) {
+        throw new Error("could not insert nothing to database");
+    }
+    const keys = _.keys(objs[0]);
+    // we should find all keys
+    _.each(objs, (obj) => {
+        const keysInObj = _.keys(obj);
+        _.each(keysInObj, (key) => {
+            if (! _.contains(keys, key)) {
+                keys.push(key);
+            }
+        });
+    });
+
+    let query = ('INSERT INTO ').concat(table_name).concat(' (');
+    _.each(keys, (key) => {
+        query = query.concat(key).concat(',');
+    });
+    query = query.slice(0, -1);
+    query = query.concat(') VALUES ');
+
+    let end_query = keys.reduce((res, key) => {
+        return res.concat('?,');
+    }, "");
+    end_query = end_query.slice(0, -1);
+    end_query = "(".concat(end_query).concat("),");
+    var values = [];
+    _.each(objs, (obj) => {
+        query = query.concat(end_query);
+        _.each(keys, (key) => {
+            values.push(obj[key]);
+        });
+    });
+    query = query.slice(0, -1);
+    return mysql.format(query, values);
 };
