2a55ab034d11570e6965dd8bee3e4cdcf61a9853
Oscar Garay <ojcgv21@gmail.com>
2018-04-27 17:40:14 +0200

    updated sql framework

--- utils/sql.js
+++ utils/sql.js
@@ -1,102 +1,109 @@
-const _ = require("underscore");
+const _ = require('underscore');
 const mysql = require("promise-mysql");
+const debugLogger = require("./debugLogger.js");
 
-const isId = (field) => {
-    return field.name.slice(0, 2) === "id";
-};
+const isId = field => field.name.slice(0, 2) === "id";
 
-const isFk = (field) => {
-    return field.name.slice(0, 2) === "fk";
-};
+const isFk = field => field.name.slice(0, 2) === "fk";
 
 const buildColumn = (field) => {
     if (isId(field)) {
-        return '\n '.concat(field.name).concat(' INT NOT NULL AUTO_INCREMENT,');
+        return `${field.name} INT NOT NULL AUTO_INCREMENT`;
     } else if (isFk(field)) {
-        return '\n '.concat(field.name).concat(' ').concat('INT') + (field.notNull ? ' NOT NULL,' : ' NULL,');
+        return `${field.name}${field.complement ? field.complement : ''} INT ${field.notNull ? 'NOT NULL' : 'NULL'}`;
     } else {
-        return '\n '.concat(field.name).concat(' ').concat(field.type) + (field.notNull ? ' NOT NULL,' : ' NULL,');
+        return `${field.name} ${field.type} ${field.notNull ? 'NOT NULL' : 'NULL'}`
+                .concat(field.default ? ` DEFAULT ${field.default}`  : '');
     }
 };
 
-const pk = (pkName, query) => {
-    return (pkName ? query.concat('\n '.concat('PRIMARY KEY ( ').concat(pkName).concat(' ),')) : query);
-};
+const pk = (pkName) => mysql.format('PRIMARY KEY ( ?? )', pkName);
 
-const fks = (fkNames, query) => {
-    fkNames.forEach((fk) => {
-        var fk_table = fk.substring(3);
-        var fk_id = 'id_'.concat(fk_table);
-        query = query.concat('\n ')
-                    .concat('CONSTRAINT FOREIGN KEY (').concat(fk).concat(')')
-                    .concat(' REFERENCES ').concat(fk_table)
-                    .concat(' ( ').concat(fk_id).concat(' ),');
-    });
-    return query;
-};
+const fksF = (fks) => fks.map((fk) => {
+    var fk_table = fk.name.substring(3);
+    var fk_id = `id_${fk_table}`;
+    return mysql.format(
+            'CONSTRAINT FOREIGN KEY (??) REFERENCES ?? ( ?? )',
+            [fk.name + (fk.complement ? fk.complement : ''), fk_table, fk_id]);
+});
 
-exports.createTableQuery = (table) => {
-    let query = '';
-    query = query.concat('CREATE TABLE IF NOT EXISTS ').concat(table.table_name).concat('  (');
-    let pkName;
-    const fkNames = [];
-    table.fields.forEach((field) => {
+const buildColumnList = (fieldList) => {
+    var pkName;
+    const fks = [];
+    const columnList = [];
+    fieldList.forEach((field) => {
         if (isId(field)) {
             pkName = field.name;
         } else if (isFk(field)) {
-            fkNames.push(field.name);
+            fks.push(field);
         }
-        query = query.concat(buildColumn(field));
+        columnList.push(buildColumn(field));
     });
-    query = pk(pkName, query);
-    query = fks(fkNames, query);
-    query = query.slice(0, -1);
-    query = query.concat(')');
-    console.info(query);
+    if (pkName) {
+        columnList.push( pk(pkName) );
+    }
+    Array.prototype.push.apply(columnList, fksF(fks));
+    return columnList.join(',\n ');
+};
+
+exports.pmap = (array = [], iterator, { concurrency = 1000 } = {}) => {
+    const size = array instanceof Array ? array.length : _.size(array);
+    return new Promise((resolve, reject) => {
+        var runner = (function* () {
+            var promises = [], p;
+            //step 1: start a new promise every time runner.next is called
+            for (var key in array) {
+                promises.push(p = iterator(array[key], key));
+                if (size >= concurrency) {
+                    p.catch(reject).then(() => runner.next()); //start the next promise when one resolves
+                }
+                yield; //execution waiting for a call to runner.next
+            }
+            //step 2: everything has been started, we just have to wait for the promises to be completed to return the results
+            Promise.all(promises).then(resolve).catch(reject);
+        })();
+        //init: start the first promises
+        var i = 0;
+        while (i < concurrency && ! runner.next().done) {
+            ++i;
+        }
+    });
+};
+
+exports.createTableQuery = (table) => {
+    const query = mysql.format(`CREATE TABLE IF NOT EXISTS ?? (${buildColumnList(table.fields)})`, table.table_name);
+    debugLogger.log(query);
     return query;
 };
+
+const mergeKeys = (objs) => _.reduce(
+    objs,
+    (keys, obj) => _.union(keys, _.keys(obj)),
+    []);
+
+const spanValue = (obj, keys) => keys.map(key => obj[key]);
 
 exports.insertIntoQuery = (table_name, objs) => {
     if (objs.length === 0) {
         throw new Error("could not insert nothing to database");
     }
-    const keys = _.keys(objs[0]);
-    // we should find all keys
-    _.each(objs, (obj) => {
-        const keysInObj = _.keys(obj);
-        _.each(keysInObj, (key) => {
-            if (! _.contains(keys, key)) {
-                keys.push(key);
-            }
-        });
-    });
-
-    let query = ('INSERT INTO ').concat(table_name).concat(' (');
-    _.each(keys, (key) => {
-        query = query.concat(key).concat(',');
-    });
-    query = query.slice(0, -1);
-    query = query.concat(') VALUES ');
-
-    let end_query = keys.reduce((res, key) => {
-        return res.concat('?,');
-    }, "");
-    end_query = end_query.slice(0, -1);
-    end_query = "(".concat(end_query).concat("),");
-    const values = [];
-    _.each(objs, (obj) => {
-        query = query.concat(end_query);
-        _.each(keys, (key) => {
-            values.push(obj[key]);
-        });
-    });
-    query = query.slice(0, -1);
-    return mysql.format(query, values);
+    const keys = mergeKeys(objs);
+    const valueQuery = `(${new Array(keys.length).fill('?').join(',')})`;
+    const valueListQuery = new Array(objs.length).fill(valueQuery).join(',');
+    const values = _.reduce(objs,
+                        (values, obj) => values.concat(spanValue(obj, keys)),
+                        []);
+    const keyListQuery = `(${new Array(keys.length).fill('??').join(',')})`;
+    const query = mysql.format(`INSERT INTO ?? ${keyListQuery} VALUES ${valueListQuery}`,
+                    [table_name].concat(keys).concat(values));
+    // debugLogger.log(query);
+    return query;
 };
 
 exports.createDatabaseQuery = (name) => {
-    const query = 'CREATE DATABASE IF NOT EXISTS ' + name;
-    return mysql.format(query);
+    const res = mysql.format('CREATE DATABASE IF NOT EXISTS ??', name);
+    debugLogger.log(res);
+    return res;
 };
 
 exports.alterTableQuery = (data) => {
@@ -106,9 +113,10 @@ exports.alterTableQuery = (data) => {
         .concat(data.new_column.name).concat(' ')
         .concat(data.new_column.type).concat(' ')
         .concat(data.new_column.notNull ? ' NOT NULL' : ' NULL');
-    console.log(query);
+    debugLogger.log(query);
     return query;
 };
+
 
 // sw means script wrapper
 exports.sw = (func) => {
