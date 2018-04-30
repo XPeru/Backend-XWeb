f02c59c8941ddd8a1ef2a9ff5e3410d88719a1ef
Oscar Garay <ojcgv21@gmail.com>
2018-04-30 06:34:25 +0200

    #5 refacto usuario routes

--- DAO/usuarioDAO.js
+++ DAO/usuarioDAO.js
@@ -1,14 +1,44 @@
-
-var multer  =   require('multer');
-var dateGenerator = require("./dateGenerator.js");
-var dateGeneratorO = new dateGenerator("usuarioDAO");
+const dateGenerator = require("./dateGenerator.js");
+const dateGeneratorO = new dateGenerator("usuarioDAO");
+const fileOperations = require("../utils/fileOperations.js");
 // For hash encryption, used for passwords
 const md5 = require('MD5');
-var router = require("express").Router();
+const router = require("express").Router();
 
-router.get("/list", cf(async(req) => {
-	dateGeneratorO.printSelect("list");
-	var query = "SELECT " + "\n" +
+const createUsuarioAsync = async (req) => {
+
+	const usuario = {
+			nombre: req.body.NOMBRE,
+			apellidos: req.body.APELLIDOS,
+			// password encryptation should be done twice, at front and backend -> TODO
+			password: md5(req.body.PASSWORD),
+			fk_tipo_usuario: req.body.FK_TIPO_USUARIO,
+			imagen: req.body.FOTO
+	};
+	const query = sqlTools.insertIntoQuery('usuario', [usuario]);
+	dateGeneratorO.printInsert(query);
+	const connection = await mySqlPool.getConnection();
+	await connection.query(query);
+	connection.release();
+	return "OK";
+}
+
+// this should prevent SQL injection
+const getArguments = (req) => {
+	const queryParams = req.query;
+    const filter = queryParams.filter || '';
+    const sortOrder = queryParams.sortOrder;
+    const pageNumber = parseInt(queryParams.pageNumber) || 0;
+    const pageSize = parseInt(queryParams.pageSize);
+	const orderBy = queryParams.active || 'ID_USUARIO';
+	return { filter, sortOrder, pageNumber, pageSize, orderBy };
+};
+
+const getUsuariosAsync = async (req) => {
+	const connection = await mySqlPool.getConnection();
+	const { filter, sortOrder, pageNumber, pageSize, orderBy } = getArguments(req);
+
+	const query = "SELECT " + "\n" +
 				"	us.ID_USUARIO, " + "\n" +
 				"	us.NOMBRE, " + "\n" +
 				"	us.APELLIDOS, " + "\n" +
@@ -24,53 +54,91 @@ router.get("/list", cf(async(req) => {
 				"INNER JOIN" + "\n" +
 				"	TIPO_USUARIO tipo ON" + "\n" +
 				"		tipo.ID_TIPO_USUARIO = us.FK_TIPO_USUARIO" + "\n" +
-				"WHERE" + "\n" +
-				"	us.IS_ACTIVE = '1'";
-	var table = [];
-	query = mysql.format(query, table);
+				(filter ? " WHERE us.NOMBRE LIKE '%" + filter + "%'" : "") + "\n" +
+				" ORDER BY " + orderBy + " " + sortOrder;
+
 	dateGeneratorO.printSelect(query);
-	var connection = await mySqlPool.getConnection();
 	const rows = await connection.query(query);
 	connection.release();
-	return rows;
-}));
+	const initialPos = pageNumber * pageSize;
+    return rows.slice(initialPos, initialPos + pageSize);
+};
 
-router.post("/", cf( async(req) => {
-	dateGeneratorO.printInsert("/");
-	const usuario = {
-			nombre: req.body.NOMBRE,
-			apellidos: req.body.APELLIDOS,
-			password: md5(req.body.PASSWORD),
-			fk_tipo_usuario: req.body.FK_TIPO_USUARIO,
-			imagen: req.body.FOTO
-	};
-	const query = sqlTools.insertIntoQuery('usuario', [usuario]);
-	dateGeneratorO.printInsert(query);
-	var connection = await mySqlPool.getConnection();
-	await connection.query(query);
-	var result = {
-		Message: "OK"
-	};
-	connection.release();
-	return result;
-}));
-
-router.get("/:id_usuario", cf( async(req) => {
-	dateGeneratorO.printSelect(" :id_usuario");
-	var query = "CALL SP_SEARCH('USUARIO','ID_USUARIO',?)";
-	var table = [req.params.id_usuario];
-	query = mysql.format(query, table);
+const getUsuarioByIdAsync = async (req) => {
+	let query = "CALL SP_SEARCH('USUARIO','ID_USUARIO',?)";
+	query = mysql.format(query, [req.params.id_usuario]);
 	dateGeneratorO.printSelect(query);
-	var connection = await mySqlPool.getConnection();
-	var rows = await connection.query(query);
-	var result = {
-		Users: rows[0]
-	};
+	const connection = await mySqlPool.getConnection();
+	const rows = await connection.query(query);
 	connection.release();
-	return result;
-}));
+	return rows[0];
+};
 
-router.get("/authentication/:usuario_email/:usuario_password", cf( async(req) => {
+const countUsuariosAsync = async (req) => {
+	const connection = await mySqlPool.getConnection();
+	const query = "SELECT COUNT(*) AS countUsuarios FROM usuario " + (req.query.filter ? " WHERE nombre LIKE '%" + req.query.filter + "%'" : "");
+	const rows = await connection.query(query);
+	connection.release();
+	return rows[0].countUsuarios;
+};
+
+const updateUsuarioAsync = async (req) => {
+	let query = "UPDATE " + "\n" +
+				"	USUARIO" + "\n" +
+				"SET" + "\n" +
+				"	PASSWORD = ?, " + "\n" +
+				"	FK_TIPO_USUARIO = ?, " + "\n" +
+				"	IMAGEN = ?, " + "\n" +
+				"	UPDATE_TIME = CURRENT_TIMESTAMP" + "\n" +
+				"WHERE" + "\n" +
+				"	ID_USUARIO = ?";
+	const table = [md5(req.body.PASSWORD),
+				req.body.FK_TIPO_USUARIO,
+				req.body.FOTO,
+				req.body.ID_USUARIO];
+	query = mysql.format(query, table);
+	dateGeneratorO.printUpdate(query);
+	const connection = await mySqlPool.getConnection();
+	await connection.query(query);
+	connection.release();
+	return "OK";
+};
+
+const uploadUsuarioImageAsync = async (req) => {
+	const pathFromFrontend = "/usuario-imagen/";
+    const pathFromBackend = "./public/medias/usuario/";
+    const data = {
+        //this path has to exist before running the server
+        path: pathFromBackend,
+        end_name: '-' + Date.now() + '.jpg',
+        //this must be the same in Service Angular
+        base_name: "usuario"
+      };
+    const nameFile = await fileOperations.uploadFileAsync(req, res, data);
+    return pathFromFrontend + nameFile;
+};
+
+const desactivateUsuarioAsync = async (req) => {
+	let query = "UPDATE" + "\n" +
+				"	USUARIO" + "\n" +
+				"SET" + "\n" +
+				"	IS_ACTIVE = ?," + "\n" +
+				"	UPDATE_TIME = CURRENT_TIMESTAMP" + "\n" +
+				"WHERE" + "\n" +
+				"	ID_USUARIO = ?";
+
+	const table = [false, req.body.ID_USUARIO];
+	query = mysql.format(query, table);
+	dateGeneratorO.printUpdate(query);
+	const connection = await mySqlPool.getConnection();
+	await connection.query(query);
+	connection.release();
+	return "OK";
+}
+
+
+
+router.get("/authentication/:usuario_email/:usuario_password", cf(async(req) => {
 	dateGeneratorO.printSelect("/authentication/:usuario_email/:usuario_password");
 	var query = "SELECT " + "\n" +
 				"	* " + "\n" +
@@ -93,89 +161,13 @@ router.get("/authentication/:usuario_email/:usuario_password", cf( async(req) =>
 	return result;
 }));
 
-router.put("/", cf( async(req) => {
-	dateGeneratorO.printUpdate("/");
-	var query = "UPDATE " + "\n" +
-				"	USUARIO" + "\n" +
-				"SET" + "\n" +
-				"	PASSWORD = ?, " + "\n" +
-				"	FK_TIPO_USUARIO = ?, " + "\n" +
-				"	FOTO = ?, " + "\n" +
-				"	UPDATE_TIME = CURRENT_TIMESTAMP" + "\n" +
-				"WHERE" + "\n" +
-				"	ID_USUARIO = ?";
-	var table = [md5(req.body.PASSWORD),
-				req.body.FK_TIPO_USUARIO,
-				req.body.FOTO,
-				req.body.ID_USUARIO];
-	query = mysql.format(query, table);
-	dateGeneratorO.printUpdate(query);
-	var connection = await mySqlPool.getConnection();
-	await connection.query(query);
-	var result = {
-		Message: "OK"
-	};
-	connection.release();
-	return result;
-}));
-
-router.put("/delete", cf( async(req) => {
-	dateGeneratorO.printUpdate("delete");
-	var query = "UPDATE" + "\n" +
-				"	USUARIO" + "\n" +
-				"SET" + "\n" +
-				"	IS_ACTIVE = ?," + "\n" +
-				"	UPDATE_TIME = CURRENT_TIMESTAMP" + "\n" +
-				"WHERE" + "\n" +
-				"	ID_USUARIO = ?";
-	var table = [!req.body.IS_ACTIVE,
-				req.body.ID_USUARIO];
-	query = mysql.format(query, table);
-	dateGeneratorO.printUpdate(query);
-	var connection = await mySqlPool.getConnection();
-	await connection.query(query);
-	var result = {
-		Message: "OK"
-	};
-	connection.release();
-	return result;
-}));
-
-const uploadAsync = async (req, res, data) => {
-    let completePath;
-    const storage = multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, data.path);
-        },
-        filename: (req, file, cb) => {
-            const name = file.fieldname + data.end_name;
-            completePath = data.path + name;
-            cb(null, name);
-        }
-    });
-    const upload = multer({ storage: storage }).single(data.base_name);
-
-    return new Promise((resolve, reject) => {
-        upload(req, res, (error) => {
-            if (error) {
-                reject(error);
-                return res.end(error);
-            } else {
-                resolve(completePath);
-            }
-        });
-    });
-};
-
-router.post("/photo", cf(async (req, res) => {
-  const data = {
-    //this path has to exist before running the server
-    path: "./media/usuarios/",
-    end_name: '-' + Date.now(),
-    base_name: "userPhoto"
-  };
-  const completePath = await uploadAsync(req, res, data);
-  return completePath;
-}));
+router.post('/create', cf(createUsuarioAsync));
+router.post('/image', cf(uploadUsuarioImageAsync));
+router.put('/update', cf(updateUsuarioAsync));
+router.put('/desactive', cf(desactivateUsuarioAsync));
+router.get('/count', cf(countUsuariosAsync));
+router.get('/get', cf(getUsuariosAsync));
+router.get('/get/:id', cf(getUsuarioByIdAsync));
+//router.get('/getActive', cf(getActiveUsuariosAsync));
 
 exports.router = router;
