fb1078058880d64f5ff324ef945eaf39a23c1118
eduapb <eduardopaucar1@gmail.com>
2018-04-30 15:36:24 -0500

    #8: Refactoring

--- DAO/almacenDAO.js
+++ DAO/almacenDAO.js
@@ -1,86 +1,64 @@
-/* global mySqlPool, mysql */
-var dateGenerator = require("./dateGenerator.js");
-var dateGeneratorO = new dateGenerator("almacenDAO");
-var router = require("express").Router();
+const dateGenerator = require("./dateGenerator.js");
+const dateGeneratorO = new dateGenerator("almacenDAO");
+const router = require("express").Router();
 
-dateGeneratorO.printStart();
-
-router.get("/list", cf( async(req) => {
-	dateGeneratorO.printSelect("list");
-	var query = "CALL SP_SEARCH_ALL('ALMACEN')";
-	var table = [];
-	query = mysql.format(query, table);
+const getAlmacenesAsync = async (req) => {
+	const query = "CALL SP_SEARCH_ALL('ALMACEN')";
 	dateGeneratorO.printSelect(query);
-	var connection = await mySqlPool.getConnection();
-	var rows = await connection.query(query);
-	var result = {
-		Almacenes: rows[0]
-	};
+	const connection = await mySqlPool.getConnection();
+	const rows = await connection.query(query);
 	connection.release();
-	return result;
-}));
+	return rows[0];
+};
 
-router.post("/", cf( async(req) => {
-	dateGeneratorO.printInsert();
-	var query = "INSERT INTO " + "\n" +
-				"	ALMACEN (" + "\n" +
-				"		CODIGO_ALMACEN," + "\n" +
-				"		UBICACION" + "\n" +
-				"	)" + "\n" +
-				"VALUES (" + "\n" +
-				"	?, " + "\n" +
-				"	?" + "\n" +
-				")";
-	var table = [req.body.CODIGO_ALMACEN,
-				req.body.UBICACION];
-	query = mysql.format(query, table);
+const createAlmacenAsync = async (req) => {
+	const almacen = {
+		codigo_almacen: req.body.CODIGO_ALMACEN,
+		ubicacion: req.body.UBICACION
+	}
+	const query = sqlTools.insertIntoQuery('almacen',[almacen]);
 	dateGeneratorO.printInsert(query);
-	var connection = await mySqlPool.getConnection();
+	const connection = await mySqlPool.getConnection();
 	await connection.query(query);
-	var result = {
-		Message: "OK"
-	};
 	connection.release();
-	return result;
-}));
+	return "OK";
+};
 
-router.put("/", cf( async(req) => {
-	dateGeneratorO.printUpdate();
-	var query = "UPDATE" + "\n" +
+const updateAlmacenAsync = async (req) => {
+	let query = "UPDATE" + "\n" +
 				"	ALMACEN " + "\n" +
 				"SET" + "\n" +
 				"	CODIGO_ALMACEN = ?, " + "\n" +
 				"	UBICACION = ? " + "\n" +
 				"WHERE " + "\n" +
 				"	ID_ALMACEN = ?";
-	var table = [req.body.CODIGO_ALMACEN, req.body.UBICACION, req.body.ID_ALMACEN];
+	const table = [	req.body.CODIGO_ALMACEN,
+									req.body.UBICACION,
+									req.body.ID_ALMACEN];
 	query = mysql.format(query, table);
 	dateGeneratorO.printUpdate(query);
-	var connection = await mySqlPool.getConnection();
+	const connection = await mySqlPool.getConnection();
 	await connection.query(query);
-	var result = {
-		Message: "OK"
-	};
 	connection.release();
-	return result;
-}));
+	return "OK";
+};
 
-router.delete("/:id_almacen", cf( async(req) => {
-	dateGeneratorO.printDelete("/:id_almacen");
-	var query = "DELETE FROM" + "\n" +
+const deleteAlmacenById = async (req) => {
+	let query = "DELETE FROM" + "\n" +
 				"	ALMACEN" + "\n" +
 				"WHERE " + "\n" +
 				"	ID_ALMACEN = ?";
-	var table = [req.params.id_almacen];
-	query = mysql.format(query, table);
+	query = mysql.format(query, [req.params.id_almacen]);
 	dateGeneratorO.printDelete(query);
-	var connection = await mySqlPool.getConnection();
+	const connection = await mySqlPool.getConnection();
 	await connection.query(query);
-	var result = {
-		Message: "OK"
-	};
 	connection.release();
-	return result;
-}));
+	return "OK";
+};
+
+router.get('/get', cf(getAlmacenesAsync));
+router.post('/create', cf(createAlmacenAsync));
+router.put('/update', cf(updateAlmacenAsync));
+router.delete('/delete/:id', cf(deleteAlmacenById));
 
 exports.router = router;
