5c905476c41ea933b6c20b375774e061b8d93cf2
Oscar Garay <ojcgv21@gmail.com>
2018-07-22 17:56:16 +0200

    using colors for debugLogger

--- utils/debugLogger.js
+++ utils/debugLogger.js
@@ -1,5 +1,16 @@
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+const colors = require('colors');
+
 exports.log = (line) => {
-    if (process.env.DEBUG == 'true') {
-        console.log(line);
-    }
+	if (process.env.DEBUG === 'true') {
+		console.warn(line);
+	}
+};
+
+exports.glog = (line) => {
+	console.warn(colors.green(line));
+};
+
+exports.logError = (line) => {
+	console.error(colors.red(line));
 };
