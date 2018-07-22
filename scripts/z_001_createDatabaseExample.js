005afc72b1cc5b74aacdb3f9c5ff1d923df24128
Oscar Garay <ojcgv21@gmail.com>
2018-07-22 22:59:44 +0200

    eslint scripts

--- scripts/z_001_createDatabaseExample.js
+++ scripts/z_001_createDatabaseExample.js
@@ -1,11 +1,10 @@
-const createConnection = require("../utils/standalone.js");
-const sqlTools = require("../utils/sql.js");
+const createConnection = require('../utils/standalone.js');
+const sqlTools = require('../utils/sql.js');
 
 sqlTools.sw(async () => {
-    const connection = await createConnection.connection();
-    const query = sqlTools.createDatabaseQuery("Test");
-    console.log(query);
-    await connection.query(query);
-    await connection.commit();
-    await connection.end();
+	const connection = await createConnection.connection();
+	const query = sqlTools.createDatabaseQuery('Test');
+	await connection.query(query);
+	await connection.commit();
+	await connection.end();
 });
--- scripts/z_002_changeToImage.js
+++ scripts/z_002_changeToImage.js
@@ -1,21 +1,21 @@
-const createConnection = require("../utils/standalone.js");
-const sqlTools = require("../utils/sql.js");
+const createConnection = require('../utils/standalone.js');
+const sqlTools = require('../utils/sql.js');
 
 sqlTools.sw(async () => {
-    const connection = await createConnection.connection();
-    await connection.query('USE ' + 'testdb');
+	const connection = await createConnection.connection();
+	await connection.query('USE ' + 'testdb');
 
-    const data = {
-        table_name: "USUARIO",
-        old_column_name: "FOTO",
-        new_column: {
-            name: "IMAGEN",
-            type: "VARCHAR(255)",
-            notNull: true
-        }
-    };
+	const data = {
+		table_name: 'USUARIO',
+		old_column_name: 'FOTO',
+		new_column: {
+			name: 'IMAGEN',
+			type: 'VARCHAR(255)',
+			notNull: true
+		}
+	};
 
-    await connection.query(sqlTools.alterTableQuery(data));
-    await connection.commit();
-    await connection.end();
+	await connection.query(sqlTools.alterTableQuery(data));
+	await connection.commit();
+	await connection.end();
 });
