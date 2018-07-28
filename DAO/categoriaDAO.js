da2f5c0777a4a16c97726bc0a9fbfc723152faa5
eduapb <eduardopaucar1@gmail.com>
2018-07-28 11:05:02 +0900

    #10 Categoria Articulo DAO refacto

--- DAO/categoriaDAO.js
+++ DAO/categoriaDAO.js
@@ -1,83 +1,79 @@
-/* global mySqlPool, mysql, cf */
-var dateGenerator = require('./dateGenerator.js');
-var dateGeneratorO = new dateGenerator('categoriaDAO');
-var router = require('express').Router();
+const dateGenerator = require('./dateGenerator.js');
+const dateGeneratorO = new dateGenerator('categoriaarticuloDAO');
+const router = require('express').Router();
 
 dateGeneratorO.printStart();
 
-router.get('/list', cf(async () => {
-	dateGeneratorO.printSelect('/list');
-	var query = "CALL SP_SEARCH_ALL('CATEGORIA')";
-	var table = [];
-	query = mysql.format(query, table);
-	dateGeneratorO.printSelect(query);
-	var connection = await mySqlPool.getConnection();
-	var rows = await connection.query(query);
-	var result = {
-		Categorias: rows[0]
-	};
-	connection.release();
-	return result;
-}));
+const getArguments = (req) => {
+	const queryParams = req.query;
+	const filter = queryParams.filter || '';
+	const sortOrder = queryParams.sortOrder;
+	const pageSize = parseInt(queryParams.pageSize, 10) || 0;
+	const initialPos = (parseInt(queryParams.pageNumber, 10) || 0) * pageSize;
+	const orderBy = queryParams.active || 'DESCRIPCION';
+	return { filter, sortOrder, initialPos, pageSize, orderBy };
+};
 
-router.post('/', cf(async (req) => {
-	dateGeneratorO.printInsert('/');
-	var query = 'INSERT INTO ' + '\n' +
-				'	CATEGORIA (' + '\n' +
-				'		DESCRIPCION' + '\n' +
-				'	)' + '\n' +
-				'VALUES (' + '\n' +
-				'	?' + '\n' +
-				')';
-	var table = [req.body.DESCRIPCION];
-	query = mysql.format(query, table);
-	dateGeneratorO.printInsert(query);
-	var connection = await mySqlPool.getConnection();
-	await connection.query(query);
-	var result = {
-		Message: 'OK'
-	};
+const getCategoriasAsync = async (req) => {
+	const connection = await mySqlPool.getConnection();
+	const { filter, sortOrder, initialPos, pageSize, orderBy } = getArguments(req);
+	const query = 'SELECT * FROM CATEGORIA ' +
+		(filter ? ' WHERE DESCRIPCION LIKE "%' + filter + '%"' : '') +
+		' ORDER BY ' + orderBy +
+		' ' + sortOrder +
+		'  LIMIT ' + initialPos + ', ' + pageSize;
+	const rows = await connection.query(query);
 	connection.release();
-	return result;
-}));
+	return rows;
+};
 
-router.put('/', cf(async (req) => {
-	dateGeneratorO.printUpdate('/');
-	var query = 'UPDATE' + '\n' +
-				'	CATEGORIA ' + '\n' +
-				'SET' + '\n' +
-				'	DESCRIPCION = ? ' + '\n' +
-				'WHERE ' + '\n' +
-				'	ID_CATEGORIA = ?';
-	var table = [req.body.DESCRIPCION,
-		req.body.ID_CATEGORIA];
-	query = mysql.format(query, table);
-	dateGeneratorO.printUpdate(query);
-	var connection = await mySqlPool.getConnection();
-	await connection.query(query);
-	var result = {
-		Message: 'OK'
-	};
+const countCategoriasAsync = async (req) => {
+	const connection = await mySqlPool.getConnection();
+	const query = 'SELECT COUNT(*) AS countCategorias FROM CATEGORIA ' + (req.query.filter ? ' WHERE DESCRIPCION LIKE "%' + req.query.filter + '%"' : '');
+	const rows = await connection.query(query);
 	connection.release();
-	return result;
-}));
+	return rows[0].countCategorias;
+};
 
-router.delete('/:id_categoria', cf(async (req) => {
-	dateGeneratorO.printDelete('/:id_categoria');
-	var query = 'DELETE FROM' + '\n' +
-				'	CATEGORIA' + '\n' +
-				'WHERE ' + '\n' +
-				'	ID_CATEGORIA = ?';
-	var table = [req.params.id_categoria];
-	query = mysql.format(query, table);
-	dateGeneratorO.printDelete(query);
-	var connection = await mySqlPool.getConnection();
-	await connection.query(query);
-	var result = {
-		Message: 'OK'
+const createCategoriaAsync = async (req) => {
+	const categoria = {
+		DESCRIPCION: req.body.DESCRIPCION
 	};
+	const connection = await mySqlPool.getConnection();
+	await connection.query(sqlTools.insertIntoQuery('CATEGORIA', [categoria]));
 	connection.release();
-	return result;
-}));
+	return 'OK';
+};
+
+const updateCategoriaAsync = async (req) => {
+	const columns = ['DESCRIPCION'];
+	const connection = await mySqlPool.getConnection();
+	await connection.query(sqlTools.updateQuery('CATEGORIA', req.body, columns));
+	connection.release();
+	return 'OK';
+};
+
+const getCategoriaByIdAsync = async (req) => {
+	const connection = await mySqlPool.getConnection();
+	const id_categoria_articulo = req.params.id;
+	const query = 'SELECT * FROM CATEGORIA WHERE ID_CATEGORIA = ?';
+	const result = await connection.query(mysql.format(query, id_categoria_articulo));
+	connection.release();
+	return result[0];
+};
+
+const deleteCategoriaAsync = async (req) => {
+	var connection = await mySqlPool.getConnection();
+	await connection.query(sqlTools.deleteQuery('CATEGORIA', req.params));
+	connection.release();
+	return 'OK';
+};
+
+router.get('/get', cf(getCategoriasAsync));
+router.post('/create', cf(createCategoriaAsync));
+router.put('/update', cf(updateCategoriaAsync));
+router.get('/get/:id', cf(getCategoriaByIdAsync));
+router.delete('/delete/:id', cf(deleteCategoriaAsync));
+router.get('/count', cf(countCategoriasAsync));
 
 exports.router = router;
