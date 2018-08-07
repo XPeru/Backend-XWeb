6a42a3fa62c9dbc1ae9c0976cd56a32cb0993242
Paty Arakawa <paty.arakawa@gmail.com>
2018-08-07 13:07:09 -0500

    #17 create usuario table

---
+++ scripts/z_008_createUsuarioTable.js
@@ -0,0 +1,63 @@
+const sqlPool = require('../utils/standalone.js');
+const sqlTools = require('../utils/sql.js');
+
+const table = {
+	table_name: 'USUARIO',
+	fields: [
+		{
+			name: 'ID_USUARIO'
+		},
+		{
+			name: 'NOMBRE',
+			type: 'VARCHAR(45)',
+			notNull: true
+		},
+		{
+			name: 'APELLIDOS',
+			type: 'VARCHAR(100)',
+			notNull: true
+    },
+		{
+			name: 'EMAIL',
+			type: 'VARCHAR(100)',
+			notNull: true
+    },
+		{
+			name: 'PASSWORD',
+			type: 'VARCHAR(256)',
+			notNull: true
+    },
+		{
+			name: 'FOTO',
+			type: 'VARCHAR(100)',
+			notNull: true
+    },
+		{
+			name: 'IS_ACTIVE',
+			type: 'BOOLEAN',
+			notNull: true,
+			default: '1'
+    }, 
+		{
+			name: 'CREATE_TIME',
+			type: 'TIMESTAMP',
+			notNull: true,
+			default: 'CURRENT_TIMESTAMP'
+    },  
+		{
+			name: 'UPDATE_TIME',
+			type: 'TIMESTAMP'
+    },      
+		{
+			name: 'FK_TIPO_USUARIO'
+		}                       
+	]
+};
+
+sqlTools.sw(async () => {
+	const connection = await sqlPool.connection();
+	await connection.query('USE ' + process.env.MYSQL_DATABASE_NAME);
+	await connection.query(sqlTools.createTableQuery(table));
+	await connection.commit();
+	await connection.end();
+});
